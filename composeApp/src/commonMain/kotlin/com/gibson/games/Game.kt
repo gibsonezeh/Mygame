457c8a741fdba2eb820e77be0cbe263e918be121
user <gibsonezeh6@gmail.com>
2025-06-22 12:20:41 +0000

    done

--- composeApp/src/commonMain/kotlin/com/gibson/games/Game.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/games/Game.kt
@@ -3,6 +3,10 @@
 package com.gibson.games
 
 enum class Game(val displayName: String){
-    LUDO("Ludo Game"),
+    LUDO("Ludo 🎲 Game"),
+    CHECKERS("checkers"),
+    SNAKE("snake 🐍"),
+    CHESS("♟️ chess"),
+    TIC_TAC_TOE("TIC_TAC_TOE")
     // Add future games here
 }
